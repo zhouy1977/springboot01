0931075545c114f79cf674ac7b66a6eb63362901
zhouy1977 <727913051@qq.com>
2019-08-20 15:31:32 +0800

    第一次代码提交

---
+++ src/main/java/com/nes/bean/Person.java
@@ -0,0 +1,36 @@
+package com.nes.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "test")
+public class Person {
+
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
